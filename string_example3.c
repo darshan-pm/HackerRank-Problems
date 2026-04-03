655f1dba6bc1aec547d8b1418cc710e3b3c49225
Darshan <darshan200713@gmail.com>
2026-04-03 19:11:54 +0530

    string_example3 program

---
+++ string_example3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    char str[] = "Programming";
+    printf("%c\n", str[0]);
+    printf("%c\n", str[1]);
+    printf("%c\n", str[2]);
+    printf("%c\n", str[3]);
+    printf("%c\n", str[4]);
+    printf("%c\n", str[5]);
+    printf("%c\n", str[6]);
+
+    return 0;
+}
